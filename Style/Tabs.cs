19e551d48670e894f19ac123752d38709700b926
jjblock21 <jjblock21@gmail.com>
2021-03-09 13:43:05 +0100

    Added the "don't foget to save!" message.

--- Style/Tabs.cs
+++ Style/Tabs.cs
@@ -5,5 +5,9 @@ namespace Blueprintz.Style
     public static class Tabs
     {
         public static TabPage editorPage = new TabPage();
+        // TODO: set this to true if saved.
+        public static bool recentlySaved = true;
+
+        public static string tabName = "";
     }
 }
--- Window.cs
+++ Window.cs
@@ -3,6 +3,7 @@ using MaterialSkin.Controls;
 using System;
 using Blueprintz.Style;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Blueprintz
 {
@@ -67,36 +68,116 @@ namespace Blueprintz
             => CloseDelayed();
         #endregion
 
+        // TODO: put all of this into it's own file.
+        // TODO: Add save dialog where save is in the Editor section.
         #region Editor
         private void CreateNewEditor()
         {
-            // Add Page if it doesn't already exist
-            if (!MainControl.TabPages.Contains(Tabs.editorPage))
-                MainControl.TabPages.Add(Tabs.editorPage);
-
-            // Change Tab title
-            if (MainControl.TabPages[1].Text != "New Blueprint")
-                Tabs.editorPage.Text = "New Blueprint";
-
-            // Open Tab
-            MainControl.SelectedTab = Tabs.editorPage;
+            if (!Tabs.recentlySaved)
+                new MultichoiceMaterialMessageBox(ColorSchemes.dark_3, "Don't forget to save!",
+                    new string[2] { "Don't forget to save your work!", "Do you want to save it now?" },
+                    true, CreateNewEditorCallback, this);
+            else CreateNewEditorCallback(DialogResult.No);
         }
 
         private void LoadEditor(string fileName, string fileExtension)
         {
-            // Create string to display as Tab name.
-            string tabTitle = Utils.MiddleShortenTo(10, fileName) + fileExtension;
+            if (!Tabs.recentlySaved)
+            {
+                Tabs.tabName = Utils.MiddleShortenTo(10, fileName) + fileExtension;
+                new MultichoiceMaterialMessageBox(ColorSchemes.dark_3, "Don't forget to save!",
+                    new string[2] { "Don't forget to save your work!", "Do you want to save it now?" },
+                    true, LoadEditorCallback, this);
+            }
+            else
+            {
+                Tabs.tabName = Utils.MiddleShortenTo(10, fileName) + fileExtension;
+                LoadEditorCallback(DialogResult.No);
+            }
+        }
 
-            // Add Page if it doesn't already exist
-            if (!MainControl.TabPages.Contains(Tabs.editorPage))
-                MainControl.TabPages.Add(Tabs.editorPage);
+        private void LoadEditorCallback(DialogResult result)
+        {
+            if (result == DialogResult.No)
+            {
+                // Create string to display as Tab name.
+                string tabTitle = Tabs.tabName;
 
-            // Change Tab title
-            if (MainControl.TabPages[1].Text != tabTitle)
-                Tabs.editorPage.Text = tabTitle;
+                // Add Page if it doesn't already exist
+                if (!MainControl.TabPages.Contains(Tabs.editorPage))
+                    MainControl.TabPages.Add(Tabs.editorPage);
 
-            // Open Tab
-            MainControl.SelectedTab = Tabs.editorPage;
+                // Change Tab title
+                if (MainControl.TabPages[1].Text != tabTitle)
+                    Tabs.editorPage.Text = tabTitle;
+
+                // Reset saved variable
+                Tabs.recentlySaved = false;
+
+                // Open Tab
+                MainControl.SelectedTab = Tabs.editorPage;
+            }
+            else if (result == DialogResult.Yes)
+            {
+                // Save
+
+                // Create string to display as Tab name.
+                string tabTitle = Tabs.tabName;
+
+                // Add Page if it doesn't already exist
+                if (!MainControl.TabPages.Contains(Tabs.editorPage))
+                    MainControl.TabPages.Add(Tabs.editorPage);
+
+                // Change Tab title
+                if (MainControl.TabPages[1].Text != tabTitle)
+                    Tabs.editorPage.Text = tabTitle;
+
+                // Reset saved variable
+                Tabs.recentlySaved = false;
+
+                // Open Tab
+                MainControl.SelectedTab = Tabs.editorPage;
+            }
+            Tabs.tabName = "";
+        }
+
+        private void CreateNewEditorCallback(DialogResult result)
+        {
+            if (result == DialogResult.No)
+            {
+                // Add Page if it doesn't already exist
+                if (!MainControl.TabPages.Contains(Tabs.editorPage))
+                    MainControl.TabPages.Add(Tabs.editorPage);
+
+                // Change Tab title
+                if (MainControl.TabPages[1].Text != "New Blueprint")
+                    Tabs.editorPage.Text = "New Blueprint";
+
+                // Reset saved variable
+                Tabs.recentlySaved = false;
+
+                // Open Tab
+                MainControl.SelectedTab = Tabs.editorPage;
+            }
+            else if (result == DialogResult.Yes)
+            {
+                // Save
+
+                // Add Page if it doesn't already exist
+                if (!MainControl.TabPages.Contains(Tabs.editorPage))
+                    MainControl.TabPages.Add(Tabs.editorPage);
+
+                // Change Tab title
+                if (MainControl.TabPages[1].Text != "New Blueprint")
+                    Tabs.editorPage.Text = "New Blueprint";
+
+                // Reset saved variable
+                Tabs.recentlySaved = false;
+
+                // Open Tab
+                MainControl.SelectedTab = Tabs.editorPage;
+            }
+            Tabs.tabName = "";
         }
         #endregion
 
