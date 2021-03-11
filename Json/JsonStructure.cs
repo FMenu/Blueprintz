69d472a082448fa63ee4bef7657a2056495325ce
jjblock21 <jjblock21@gmail.com>
2021-03-11 10:28:17 +0100

    Remade the SaveJson class into JsonStructure, because there already is a SaveJson class and left comment's that explain what the class was supposed to do.

---
+++ Json/JsonStructure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blueprintz.Save
+{
+    public class JsonStructure
+    {
+        //TODO: Add Properties that emulate the JSON Structure.
+        // Not methods to write JSON, the structure that get's converted into JSON
+        // Example:
+        // private string name = "test";
+        // Get's converted to:
+        // { "name": "test" }
+        // By the JsonSerializer class
+    }
+}
