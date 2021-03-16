dae664739b5a67fce42d948d0b1dba76d41cef1b
jjblock21 <jjblock21@gmail.com>
2021-03-16 14:31:40 +0100

    Added Fireworka class and some Extensions

---
+++ Editor/Fireworks.cs
@@ -0,0 +1,80 @@
+﻿using Blueprintz.JsonStructures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Blueprintz.Editor
+{
+    public class Fireworks
+    {
+        private Dictionary<string, Firework> fireworks = new Dictionary<string, Firework>();
+        private Dictionary<string, Fuse> fuses = new Dictionary<string, Fuse>();
+        private string name, description, map;
+        public Fireworks(string name, string description, Map map)
+        {
+            this.name = name;
+            this.description = description;
+            this.map = map.ToDescriptionString();
+        }
+
+
+        public BLZJsonStructure ToBLZStructure()
+        {
+            GetValueArrays(out Firework[] fireworks, out Fuse[] fuses);
+            return new BLZJsonStructure(name, description, map, fireworks, fuses);
+        }
+
+        public void GetValueArrays(out Firework[] _fireworks, out Fuse[] _fuses)
+        {
+            KeyValuePair<string, Firework>[] fireworkPair = this.fireworks.ToArray();
+            KeyValuePair<string, Fuse>[] fusePair = this.fuses.ToArray();
+            List<Firework> fireworks = new List<Firework>();
+            List<Fuse> fuses = new List<Fuse>();
+            foreach (KeyValuePair<string, Firework> pair in fireworkPair) fireworks.Add(pair.Value);
+            foreach (KeyValuePair<string, Fuse> pair in fusePair) fuses.Add(pair.Value);
+            _fireworks = fireworks.ToArray();
+            _fuses = fuses.ToArray();
+        }
+
+        public string AddFirework(Firework firework)
+        {
+            string identifier = Guid.NewGuid().ToString();
+            fireworks.Add(identifier, firework);
+            return identifier;
+        }
+
+        public Firework GetFirework(string id) => fireworks[id];
+        public Fuse GetFuse(string id) => fuses[id];
+
+        public string AddFuse(string fromId, string toId, FuseSpeed speed)
+        {
+            if (fireworks.TryGetValue(fromId, out Firework firework) && fireworks.TryGetValue(toId, out Firework firework1))
+            {
+                Fuse fuse = new Fuse(Guid.NewGuid().ToString(), firework.id, firework1.id, (int)speed);
+                string identifier = Guid.NewGuid().ToString();
+                fuses.Add(identifier, fuse);
+                return identifier;
+            }
+            else return null;
+        }
+
+        public bool RemoveFirework(string id) => fireworks.Remove(id);
+    }
+
+    public enum FuseSpeed : uint
+    {
+        Slow = 3,
+        Medium = 2,
+        Fast = 1,
+        Instant = 0
+    }
+
+    public enum Map
+    {
+        [Description("town")]
+        Town = 1,
+        [Description("ranch")]
+        Ranch = 2
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,28 @@
+﻿using Blueprintz.Editor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Blueprintz
+{
+    public static class MapExtensions
+    {
+        public static string ToDescriptionString(this Map val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+
+    public static class EnumeratorExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this IEnumerator<T> enumerator)
+        {
+            while (enumerator.MoveNext())
+                yield return enumerator.Current;
+        }
+    }
+}
